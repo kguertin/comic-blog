24014234f8caff504a301bf9ca71795b8849a5ce
Kevin Guertin <kevin.m.guertin@gmail.com>
2021-12-20 11:53:00 -0500

    use site metadata for header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
+const year = new Date(Date.now()).getFullYear()
+
 module.exports = {
+  siteMetadata: {
+    author: "Kevin Guertin",
+    title: "Comic Cavern",
+    curretYear: year,
+  },
   plugins: ["gatsby-plugin-typescript"],
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,32 @@
 import * as React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 const Header: React.FC = () => {
+  const data = useStaticQuery(graphql`
+    query HeaderData {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
   return (
     <nav>
-      <h2>Comic Blog</h2>
+      <h2>{data.site.siteMetadata.title}</h2>
       <ul>
-        <li>Home</li>
-        <li>Reviews</li>
-        <li>Read Throughs</li>
-        <li>Community</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/community">Community</Link>
+        </li>
+        <li>
+          <Link to="/reviews">Reviews</Link>
+        </li>
+        <li>
+          <Link to="/series">Series Reads</Link>
+        </li>
       </ul>
     </nav>
   )
